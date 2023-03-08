1da698d514a67015d83c095b4c55f8e542ce02b0
Haseeb Ahmad <ahmad.has03@gmail.com>
2023-03-08 11:40:33 -0500

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ var TWILIO_SMS_NUMBER = '';
 var TWILIO_AUTH_TOKEN = '';
 var HEADER_AUTH_TOKEN = TWILIO_ACCOUNT_SID + ':' + TWILIO_AUTH_TOKEN;
 var TWILIO_WHATSAPP_NUMBER =  '';
-var TWILIO_PHONE_CALLBACK_URL = 'https://handler.twilio.com/twiml/....';
+var DEFAULT_PHONE_CALLBACK_SID = '';
+var TWILIO_PHONE_CALLBACK_BASE_URL = 'https://handler.twilio.com/twiml/';
 
 /**
  * Installs a trigger in the Spreadsheet to run upon the Sheet being opened.
@@ -29,7 +30,6 @@ function onOpen() {
       .addToUi();
 };  
 
-
 /**
  * Sends calls listed in the Google Sheet (current/active)
  *
@@ -43,7 +43,17 @@ function makeCallsToAll() {
   
   // Try to send an SMS to every row and save their status.
   rows.forEach(function(row) {
-    row[STATUS] = makeCall(row[PHONE_NUMBER],row[SOURCE_NUMBER]);
+    if (row[PHONE_NUMBER] &&
+        row[PHONE_NUMBER].toString().trim().length > 0 &&
+        row[SOURCE_NUMBER] &&
+        row[SOURCE_NUMBER].toString().trim().length > 0
+    ) {
+      var msg = row[MESSAGE].toString();
+      if (msg.trim().length == 0) {
+        msg = DEFAULT_PHONE_CALLBACK_SID;
+      }
+      row[STATUS] = makeCall(row[PHONE_NUMBER], row[SOURCE_NUMBER], msg);
+    }
   });
   
   // Write the entire data back into the sheet.
@@ -63,7 +73,15 @@ function sendSmsToAll() {
   
   // Try to send an SMS to every row and save their status.
   rows.forEach(function(row) {
-    row[STATUS] = sendSms(row[PHONE_NUMBER], row[MESSAGE], row[SOURCE_NUMBER]);
+    if (row[PHONE_NUMBER] &&
+        row[PHONE_NUMBER].toString().trim().length > 0 &&
+        row[SOURCE_NUMBER] &&
+        row[SOURCE_NUMBER].toString().trim().length > 0 &&
+        row[MESSAGE] &&
+        row[MESSAGE].toString().trim().length > 0
+    ) {
+      row[STATUS] = sendSms(row[PHONE_NUMBER], row[MESSAGE], row[SOURCE_NUMBER]);
+    }
   });
   
   // Write the entire data back into the sheet.
@@ -83,7 +101,13 @@ function sendWhatsAppToAll() {
   
   // Try to send an SMS to every row and save their status.
   rows.forEach(function(row) {
-    row[STATUS] = sendWhatsApp(row[PHONE_NUMBER], row[MESSAGE]);
+    if (row[PHONE_NUMBER] &&
+        row[PHONE_NUMBER].toString().trim().length > 0 &&
+        row[MESSAGE] &&
+        row[MESSAGE].toString().trim().length > 0
+    ) {
+      row[STATUS] = sendWhatsApp(row[PHONE_NUMBER], row[MESSAGE]);
+    }
   });
   
   // Write the entire data back into the sheet.
@@ -94,6 +118,7 @@ function sendWhatsAppToAll() {
 function containsNonLatinCodepoints(s) {
     return /[^\u0000-\u00ff]/.test(s);
 }
+
 function showAlert(message) {
   var ui = SpreadsheetApp.getUi(); // Same variations.
 
@@ -113,9 +138,10 @@ function showAlert(message) {
     return 1;
   }
 }
+
 /**
  * Sends a message to a given phone number via SMS through Twilio.
- * To learn more about sending an SMS via Twilio and Sheets: 
+ * To learn more about sending an SMS via Twilio and Sheets:
  * https://www.twilio.com/blog/2016/02/send-sms-from-a-google-spreadsheet.html
  *
  * @param {number} phoneNumber - phone number to send SMS to.
@@ -124,13 +150,10 @@ function showAlert(message) {
  */
 function sendSms(phoneNumber, message, source) {
   var cont = 0;
-
-
   var out = new Array();
   if (cont == 0) {
     var twilioUrl = 'https://api.twilio.com/2010-04-01/Accounts/' + TWILIO_ACCOUNT_SID + '/Messages.json';
     var twilioWhatsAppUrl = 'https://api.twilio.com/2010-04-01/Accounts/' + TWILIO_ACCOUNT_SID + '/Messages.json';
-    
     var fromNum = ""+source;
     
     try {
@@ -160,9 +183,10 @@ function sendSms(phoneNumber, message, source) {
   return  out;
   
 }
+
 /**
  * Sends a whatsapp message to a given phone number via whatsapp api through Twilio.
- * To learn more about sending an WhatsApp via Twilio and Sheets: 
+ * To learn more about sending an WhatsApp via Twilio and Sheets:
  * https://www.twilio.com/docs/whatsapp
  *
  * @param {number} phoneNumber - phone number to send whatsapp to.
@@ -173,12 +197,7 @@ function sendWhatsApp(phoneNumber, message) {
   var out = new Array();
   var twilioUrl = 'https://api.twilio.com/2010-04-01/Accounts/' + TWILIO_ACCOUNT_SID + '/Messages.json';
   var twilioWhatsAppUrl = 'https://api.twilio.com/2010-04-01/Accounts/' + TWILIO_ACCOUNT_SID + '/Messages.json';
-  //https://api.twilio.com/2010-04-01/Accounts/AC3edf5abb365c58c7f75520a6a11b692f/Messages.json
-  
 
-  
-  
-  
   try {
     UrlFetchApp.fetch(twilioUrl, {
       method: 'post',
@@ -200,31 +219,25 @@ function sendWhatsApp(phoneNumber, message) {
     console.error('error' + err);
     if (err) return 'error: ' + err;
   }
-  
 
   return  out;
-  
 }
 
 /**
  * expors twilio logs using api call and saves repsonse to the sheet named:  'Log Reply\'s Twilio';
- * 
+ *
  */
 function exportTwilioLogs() {
-
   var exportURL = 'https://api.twilio.com/2010-04-01/Accounts/' + TWILIO_ACCOUNT_SID + '/Messages.json?__referrer=runtime&Format=json&PageSize=500&Page=0';
-  
-   try {
+  try {
     var config =  {
       method: 'get',
       headers: {
         Authorization: 'Basic ' + Utilities.base64Encode(HEADER_AUTH_TOKEN)
       }
     };
-    
+
     var response = UrlFetchApp.fetch(exportURL,config);
-    //whatsapp:+14155238886
-//+12892756522
     var myNumbers = new Array();
     myNumbers.push("whatsapp:+14155238886");
     myNumbers.push("+12892756522");
@@ -236,28 +249,22 @@ function exportTwilioLogs() {
     var counter = 2;
     for (var i =0 ; i < data.messages.length; i++) {
       var toNum = data.messages[i].to;
-      
-        var range = sheet.getRange(counter,1);
-        range.setValue(data.messages[i].from);
-        range = sheet.getRange(counter,2);
-        range.setValue(data.messages[i].to);
-        range = sheet.getRange(counter,3);
-        range.setValue(data.messages[i].body);
+      var range = sheet.getRange(counter,1);
+      range.setValue(data.messages[i].from);
+      range = sheet.getRange(counter,2);
+      range.setValue(data.messages[i].to);
+      range = sheet.getRange(counter,3);
+      range.setValue(data.messages[i].body);
 
-        range = sheet.getRange(counter,4);
-        range.setValue(data.messages[i].date_created);
+      range = sheet.getRange(counter,4);
+      range.setValue(data.messages[i].date_created);
 
-        range = sheet.getRange(counter,5);
-        range.setValue(data.messages[i].body);
-        //date_updated
-        counter = counter + 1;
-      
-      
+      range = sheet.getRange(counter,5);
+      range.setValue(data.messages[i].body);
+      //date_updated
+      counter = counter + 1;
     }
-    
-   
-    
-    
+
     console.log( 'received logs:' + new Date());
   } catch (err) {
     console.error('error' + err);
@@ -265,20 +272,22 @@ function exportTwilioLogs() {
   }
 }
 
-
 /**
  * Sends a call to a given phone number via a call api through Twilio.
- * To learn more about sending an SMS via Twilio and Sheets: 
+ * To learn more about sending an SMS via Twilio and Sheets:
  * https://www.twilio.com/docs/voice
  *
  * @param {number} phoneNumber - phone number to send call to.
  * @param {string} source - source to send via call.
  * @return {string} status of source sent (successful sent date or error encountered).
  */
-function makeCall(phoneNumber,source) {
+function makeCall(phoneNumber,source, phone_callback_sid = '') {
   var out = new Array();
   var twilioUrl = 'https://api.twilio.com/2010-04-01/Accounts/' + TWILIO_ACCOUNT_SID + '/Calls.json';
   var fromNum = ""+source;
+  if (!phone_callback_sid || phone_callback_sid.length == 0) {
+    phone_callback_sid = DEFAULT_PHONE_CALLBACK_SID;
+  }
   try {
     var response = UrlFetchApp.fetch(twilioUrl, {
       method: 'post',
@@ -287,12 +296,12 @@ function makeCall(phoneNumber,source) {
       },
       payload: {
         To: phoneNumber.toString(),
-        Url: TWILIO_PHONE_CALLBACK_URL,
+        Url: TWILIO_PHONE_CALLBACK_BASE_URL + phone_callback_sid,
         From: fromNum
       },
     });
     var responseText = 'sent: ' + new Date() + 'response from ' + phoneNumber +":"+response.getContentText();
-    
+
     out.push(responseText );
     console.log(responseText);
   } catch (err) {
@@ -301,5 +310,4 @@ function makeCall(phoneNumber,source) {
   }
 
   return  out;
-  
 }
